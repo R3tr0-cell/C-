f93fe16d3491831a3ed585194997fb23d8b2c96d
Dylan Carroll <carr0676@algonquinlive.com>
2022-01-18 14:53:45 -0500

    Finished Constructors

--- week2.cpp
+++ week2.cpp
@@ -1,8 +1,34 @@
 
 #include<iostream>
+using namespace std;
+
+namespace cst8219
+{
+	class Vehicle {
+
+	private:
+		int numWheels;
+		int numDoors;
+
+
+		Vehicle(int w, int d) {
+			numWheels = w;
+			numDoors = d;
+
+		}
+		Vehicle(int w) {
+			Vehicle(w, 4);
+		}
+		Vehicle() {
+			Vehicle(4);
+		}
+
+
+	};
+}
 
 int main(int argc, char **argv)
 {
-   std::cout << "Hello World!" << std::endl;
+   cout << "Hello World!" << endl;
    return 0;
 }
