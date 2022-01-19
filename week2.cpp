aa6709e853acc679ddc2e9bd34133e2400bf3767
Dylan Carroll <carr0676@algonquinlive.com>
2022-01-19 16:05:33 -0500

    Fixed Class Issues

--- week2.cpp
+++ week2.cpp
@@ -2,33 +2,47 @@
 #include<iostream>
 using namespace std;
 
-namespace cst8219
-{
-	class Vehicle {
+namespace cst8219 {
+	class Vehicle;
+}
+using namespace cst8219;
+
+	class cst8219::Vehicle {
 
 	private:
 		int numWheels;
 		int numDoors;
 
-
+	public:
 		Vehicle(int w, int d) {
+			cout << "In constructor with 2 parameters" << endl;
 			numWheels = w;
 			numDoors = d;
 
 		}
 		Vehicle(int w) {
+			cout << "In constructor with 1 parameters, wheels = " << w << endl;
 			Vehicle(w, 4);
 		}
 		Vehicle() {
+			cout << "In constructor with 0 parameters" << endl;
 			Vehicle(4);
+		}
+
+		~Vehicle() {
+			cout << "in destructor" << endl;
 		}
 
 
 	};
-}
+
 
 int main(int argc, char **argv)
 {
+	Vehicle myVehicle(2,4);
+   
    cout << "Hello World!" << endl;
    return 0;
 }
+
+
